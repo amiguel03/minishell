34d467113d3c2b8209a40cffbd547b77330609ef
Alvaro Miguel Gil <amiguel-@student.42madrid.com>
2024-10-10 12:05:40 +0200

    adding part line

--- src/tools/parsing.c
+++ src/tools/parsing.c
@@ -6,9 +6,24 @@
 /*   By: amiguel- <amiguel-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/08 12:26:53 by amiguel-          #+#    #+#             */
-/*   Updated: 2024/10/08 12:51:23 by amiguel-         ###   ########.fr       */
+/*   Updated: 2024/10/10 12:03:53 by amiguel-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	last_valid_arg(t_token *token)
+{
+	t_token *prev;
+	
+	if (!token || is_type(token, CMD) || is_type(token, ARG))
+	{
+		prev = prev_sep(token, NOSKIP);
+		if (!prev || is_type(prev, END) || is_type(prev, END))
+			return(1);
+		return(0);
+	}
+	else
+		return (0);
+}
 
