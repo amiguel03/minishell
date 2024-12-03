b5b890aab6c2c89ac77de3a68418c4a5b9be0061
amiguel03 <amiguel-@student.42madrid.com>
2024-12-03 11:16:42 +0100

    free_tmini

--- src/utils/free.c
+++ src/utils/free.c
@@ -45,5 +45,26 @@ void	free_t_cmd(t_cmd **cmd)
 
 void	free_t_mini(t_mini *mini)
 {
-	
+	if (mini == NULL)
+		return ;
+	if (mini->env != NULL)
+	{
+		if (mini->env->path != NULL)
+			free(mini->env->path);
+		if (mini->env->env != NULL)
+			strstr_free(mini->env->env);
+	}
+	if (mini->flags)
+		free(mini->flags);
+	if (mini->token != NULL)
+	{
+		if (mini->token->input != NULL)
+			free(mini->token->input);
+		if (mini->token->tokens != NULL)
+			strstr_free(mini->token->tokens);
+		free(mini->token);
+	}
+	if (mini->env)
+		free(mini->env);
+	free(mini);
 }
