d529451950180d1dd57010f9d0cd4f8ff5c09147
amiguel03 <amiguel-@student.42madrid.com>
2025-03-04 10:39:46 +0100

    actualizacion

--- src/executor/manage_pid.c
+++ src/executor/manage_pid.c
@@ -37,3 +37,39 @@ int pid_creation(t_global *global, char **paths, char *argv[], t_redirection *re
 	}
 	return(wait_and_manage_exit(pid));
 }
+
+void check_first_pid(pid_t pid, int fds[2], t_cmds *curr_cmd, t_global *global)
+{
+	pid = fork();
+	if(pid == 0)
+	{
+		close (fds[0]);
+		dup2(fds[1], STDOUT_FILENO);
+		close(fds[1]);
+		execute_cmd(curr_cmd->cmd, curr_cmd->redir, global);
+		exit(EXIT_SUCCES);
+	}
+	else if (pid < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void check_second_pid(pid_t pid2, int fds[2], t_cmds *curr_cmd, t_global *global)
+{
+	pid2 = fork();
+	if(pid2 == 0)
+	{
+		close (fds[1]);
+		dup2(fds[0], STDin_FILENO);
+		close(fds[0]);
+		execute_cmd(curr_cmd->cmd, curr_cmd->redir, global);
+		exit(EXIT_SUCCESS);
+	}
+	else if (pid2 < 0)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+}
