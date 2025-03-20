ab4e509974e29a1e5724c1ed5c5eb7c82b7855c3
amiguel03 <amiguel-@student.42madrid.com>
2025-03-20 11:35:33 +0100

    feat: Añadir funciones para manejar casos especiales en el ejecutor

--- src/executor/check_cmds.c
+++ src/executor/check_cmds.c
@@ -1,30 +1,30 @@
 #include "minishell.h"
 
 int	check_or(t_count *count_tmp, t_global *global, t_cmds *curr_cmd,
-	t_token *type_tmp)
+		t_token *type_tmp)
 {
 	if (count_tmp->nbr_ors > 0 && *type_tmp == OR)
 	{
 		count_tmp->nbr_ors--;
 		ft_or(global, curr_cmd, curr_cmd->next);
 		if (global->exit_code == 0)
-			return(1);
-		if(curr_cmd->next)
+			return (1);
+		if (curr_cmd->next)
 		{
 			curr_cmd = curr_cmd->next;
 			type_tmp++;
-			while(*type_tmp != OR && *type_tmp != END)
+			while (*type_tmp != OR && *type_tmp != END)
 				type_tmp++;
 			if (*type_tmp == END)
-				return(1);
+				return (1);
 		}
-		return(1);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
 
 int	check_and(t_count *count_tmp, t_global *global, t_cmds curr_cmd,
-	t_token *type_tmp)
+		t_token *type_tmp)
 {
 	if (count_tmp->nbr_ands > 0 && *type_tmp == AND)
 	{
@@ -44,26 +44,47 @@ int	check_and(t_count *count_tmp, t_global *global, t_cmds curr_cmd,
 	return (0);
 }
 
-int check_semicolon_pipe(t_count *count_tmp, t_global *global, t_cmds *curr_cmd,
-    t_token *type_tmp)
+int	check_semicolon_pipe(t_count *count_tmp, t_global *global, t_cmds *curr_cmd,
+		t_token *type_tmp)
 {
-    if (*type_tmp == SEMICOLON || *type_tmp == PIPE)
-    {
-        if (*type_tmp == SEMICOLON)
-            ft_semicolon(global, curr_cmd, curr_cmd->next);
-        else
-            ft_pipe(global, curr_cmd, curr_cmd->next);
-        
-        if (curr_cmd->next)
-        {
-            curr_cmd = curr_cmd->next;
-            type_tmp++;
-            while (*type_tmp != *type_tmp && *type_tmp != END)
-                type_tmp++;
-            if (*type_tmp == END)
-                return (1);
-        }
-        return (1);
-    }
-    return (0);
+	if (*type_tmp == SEMICOLON || *type_tmp == PIPE)
+	{
+		if (*type_tmp == SEMICOLON)
+			ft_semicolon(global, curr_cmd, curr_cmd->next);
+		else
+			ft_pipe(global, curr_cmd, curr_cmd->next);
+		if (curr_cmd->next)
+		{
+			curr_cmd = curr_cmd->next;
+			type_tmp++;
+			while (*type_tmp != *type_tmp && *type_tmp != END)
+				type_tmp++;
+			if (*type_tmp == END)
+				return (1);
+		}
+		return (1);
+	}
+	return (0);
 }
+
+int	check_special(t_count *count_tmp, t_global *global, t_cmds *curr_cmd,
+		t_token *type_tmp)
+{
+	if (*type_tmp == INPUT || *type_tmp == HEREDOC || *type_tmp == APPEND
+		|| *type_tmp == OUTPUT)
+	{
+		if (count_tmp->nbr_inputs > 0 && *type_tmp == INPUT)
+			count_tmp->nbr_inputs--;
+		else if (count_tmp->nbr_heredocs > 0 && *type_tmp == HEREDOC)
+			count_tmp->nbr_heredocs--;
+		else if (count_tmp->nbr_appends > 0 && *type_tmp == APPEND)
+			count_tmp->nbr_appends--;
+		else if (count_tmp->nbr_outputs > 0 && *type_tmp == OUTPUT)
+			count_tmp->nbr_outputs--;
+		ft_redir(global, curr_cmd);
+		return (1);
+	}
+	return (0);
+}
+
+int check_
